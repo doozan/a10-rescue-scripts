5d76b8eca945bf9b1c004ac45ba54a418eb1a521
jeff <jeff@tcbdev.tcbnet>
2012-07-24 21:39:19 -0400

    Initial checkin

---
+++ autorun-deviceinfo.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# This script will dump diagnostic information to /sysconfig/log/deviceinfo.log
+
+LOG=/mnt/sysconfig/log/deviceinfo.log
+
+mount -o remount,rw /mnt/sysconfig
+if [ ! -d /mnt/sysconfig/log ]; then
+  mkdir /mnt/sysconfig/log
+fi
+
+date > $LOG
+echo "================== /proc/partitions =================" >> $LOG
+cat /proc/partitions >> $LOG
+echo "" >> $LOG
+
+echo "================== /proc/devices =================" >> $LOG
+cat /proc/devices >> $LOG
+echo "" >> $LOG
+
+mkdir /tmp/nand
+for dev in nanda
+do
+  echo "================== $dev =================" >> $LOG
+  mount /dev/$dev /tmp/nand
+  cd /tmp/nand
+  find >> $LOG
+  cd /
+  umount /tmp/nand
+  echo "" >> $LOG
+done
+
+echo "================== /nandh strings =================" >> $LOG
+dd if=/dev/nandh of=/tmp/env bs=1024 count=128
+strings /tmp/env >> $LOG
+echo "" >> $LOG
+
+echo "================== ifconfig =================" >> $LOG
+ifconfig >> $LOG
+echo "" >> $LOG
+
+echo "================== dmesg =================" >> $LOG
+dmesg >> $LOG
+echo "" >> $LOG
+
+mount -o remount,rw /mnt/sysconfig
---
+++ autorun-extract-system-bin.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ ! -f /mnt/sysconfig/system.bin ]; then
+  echo "Could not find /system.bin on sysconfig partition"
+  echo "Searching NAND for usable system.bin files..."
+
+  mkdir /tmp/nand
+  mount /dev/nanda /tmp/nand
+
+  if [ -f /tmp/nand/script.bin ]; then
+    # Decompile script.bin
+    /sbin/fexc -I bin -O fex /tmp/nand/script.bin /tmp/system.fex
+
+    # Extract the MAC address from the uboot environment on nandh
+    dd if=/dev/nandh of=/tmp/env bs=1024 count=128
+    mac=`strings /tmp/env | grep "^mac=" | cut -d "=" -f 2 | sed -e 's/://g'`
+
+    # Set the new MAC address and save system.bin on sysconfig
+    sed -i -e"s/^MAC = \"000000000000\"/MAC = \"$mac\"/" /tmp/system.fex
+    mount -o remount,rw /mnt/sysconfig
+    /sbin/fexc -I fex -O bin /tmp/system.fex /mnt/sysconfig/system.bin
+    mount -o remount,ro /mnt/sysconfig
+
+    umount /tmp/nand
+    echo "Created system.bin from nanda/script.bin, MAC Address = $mac"
+    echo "Rebooting system to use new system.bin"
+    sleep 5
+    reboot
+  else
+    echo "Could not find /script.bin on nanda"
+  fi
+else
+  echo "/mnt/sysconfig/system.bin already installed, exiting"
+fi
