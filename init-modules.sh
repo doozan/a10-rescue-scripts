e8335f70d7017eedd2f07646439c4e70fcf21ef0
Jeff Doozan <jeff@doozan.com>
2012-07-26 01:40:47 -0400

    Init scripts

---
+++ init-modules.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ ! -f /mnt/sysconfig/system.bin ]; then
+  echo "system.bin has not yet been extracted, exiting"
+  exit 1
+fi
+
+WIRED=`fexc -I bin -O fex /mnt/sysconfig/system.bin | grep emac_used | cut -d " " -f 3`
+
+if [ $WIRED = 1 ]; then
+  echo "Loading wired ethernet module"
+  modprobe sun4i_wemac
+else
+  echo "Loading wireless module"
+  modprobe 8192cu
+fi
